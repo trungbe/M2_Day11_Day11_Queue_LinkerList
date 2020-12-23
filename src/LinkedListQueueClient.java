f9a7555a58f146b58cbad5fe2bef372405596066
trungbe <trungnd0601@gmail.com>
2020-12-23 10:54:24 +0700

    queue linkedlist

---
+++ src/LinkedListQueueClient.java
@@ -0,0 +1,14 @@
+public class LinkedListQueueClient {
+    public static void main(String[] args) {
+        MyLinkedListQueue queue = new MyLinkedListQueue();
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        queue.enqueue(40);
+        queue.enqueue(50);
+        System.out.println("Dequeue item is " + queue.dequeue().key);
+    }
+}
